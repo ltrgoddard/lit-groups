ca601d129f7c6bc573fb89af474be43ebb33ddc4
Louis Goddard <louisgoddard@gmail.com>
2016-05-15 17:34:52 +0000

    aesthetics

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(igraph)
 library(networkD3)
+source("igraph_to_networkD3_mod.R")
 
 d <- read.csv("edges.csv")
 g <- graph.data.frame(d, directed = FALSE)
@@ -27,14 +28,13 @@ function(input, output) {
             clus <- clusters(communities)
             communities <- delete.vertices(communities, which(clus$membership %in% which(clus$csize < 5)))
             igraph_to_networkD3(communities, group = membership(clusters(communities)))
-        
         } else { igraph_to_networkD3(g, group = V(g)) }
 	})
 
     charge <- reactive({input$charge})
 
-    render <- reactive({
-        validate(need(graph()$nodes == 0, print("No connections found!")))
-        output$display <- renderForceNetwork({ forceNetwork(Links = graph()$links, Nodes = graph()$nodes, Source = "source", Target = "target", NodeID = "name", Group = "group", zoom = TRUE, bounded = FALSE, fontSize = 30, opacity = 1, charge = charge(), linkWidth = JS("function(d) { return Math.sqrt(d.value)/3; }"), colourScale = JS("d3.scale.category10()")) })
+    output$display <- renderForceNetwork({
+        validate(need(try(graph()$nodes != 0), "No connections found! Try increasing the birthdate range or decreasing the minimum connection weight."))
+        forceNetwork(Links = graph()$links, Nodes = graph()$nodes, Source = "source", Target = "target", NodeID = "name", Group = "group", zoom = TRUE, bounded = TRUE, fontSize = 30, opacity = 1, charge = charge(), linkWidth = JS("function(d) { return Math.sqrt(d.value)/3; }"), colourScale = JS("d3.scale.category10()"))
     })
 }
