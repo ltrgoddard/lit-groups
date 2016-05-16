fa266b7275d8cbda5c0f241a8c25de1fd5c1a259
Louis Goddard <louisgoddard@gmail.com>
2016-05-16 07:45:13 +0000

    link to article

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ fluidPage(title = "Constellations: mapping literary groups with graph theory",
 	sidebarPanel(
 		h3("Constellations"),
         h4("Mapping literary groups with graph theory"),
-        p("This interactive graph shows communities identified by the ", a(em("Walktrap"), href = "http://arxiv.org/abs/physics/0512106"), " algorithm in the online version of ", a(em("The Oxford Companion to Modern Poetry"), href = "http://www.oxfordreference.com/view/10.1093/acref/9780199640256.001.0001/acref-9780199640256"), "(2nd edn). Connections are based on links between entries."),
+        p("This interactive graph shows communities identified by the ", a(em("Walktrap"), href = "http://arxiv.org/abs/physics/0512106"), " algorithm in the online version of ", a(em("The Oxford Companion to Modern Poetry"), href = "http://www.oxfordreference.com/view/10.1093/acref/9780199640256.001.0001/acref-9780199640256"), "(2nd edn). It accompanies an ", a("academic preprint", href = "https://github.com/ltrgoddard/lit-groups/raw/master/lit-groups.pdf"), " on the subject. Connections are based on links between entries."),
         p(strong("Click and drag"), " to move the graph, ", strong("mouse-over"), " nodes to view the people that they represent, and ", strong("pinch or use the scroll wheel"), " to zoom."),
 		sliderInput("birthdate", "Birthdate range", min = 1900, max = 2000, value = c(1940, 1965), step = 5),
         sliderInput("cutoff", "Minimum connection weight (based on closeness in age)", min = 0, max = 1, value = 0.75, step = 0.05),
